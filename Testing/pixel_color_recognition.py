1b3f8a23542370b64c7e57a36c206637c3173f1a
Hubert <hrozmarynowski776@gmail.com>
2021-11-06 22:52:43 +0100

    Update pixel_color_recognition.py

--- Testing/pixel_color_recognition.py
+++ Testing/pixel_color_recognition.py
@@ -1,9 +1,16 @@
 from ctypes import windll
+import time
+
 dc= windll.user32.GetDC(0)
 
 def getpixel(x,y):
     return tuple(int.to_bytes(windll.gdi32.GetPixel(dc,x,y), 3, 'little'))
 
 
-print(getpixel(951,280))
-print(getpixel(954, 335))
+
+time.sleep(1)
+print(getpixel(111,46))
+time.sleep(1)
+print(getpixel(111,46))
+time.sleep(1)
+print(getpixel(111,46))
