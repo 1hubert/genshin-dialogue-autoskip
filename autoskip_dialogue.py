5ca775dedae2b496a4534b0e340d102e0644bae7
Hubert Rozmarynowski <hrozmarynowski776@gmail.com>
2023-10-22 21:39:05 +0200

    improve docs

--- autoskip_dialogue.py
+++ autoskip_dialogue.py
@@ -15,7 +15,7 @@ os.system('cls')
 load_dotenv()
 print('Welcome to Genshin Impact Dialogue Skipper\n')
 
-# Check if either screen dimension is not yet set in .env
+# Check if either screen dimension is missing from .env
 if os.environ['WIDTH'] == '' or os.environ['HEIGHT'] == '':
     # Detect and set screen dimensions
     SCREEN_WIDTH = GetSystemMetrics(0)
@@ -42,35 +42,30 @@ else:
     SCREEN_WIDTH = int(os.getenv('WIDTH'))
     SCREEN_HEIGHT = int(os.getenv('HEIGHT'))
 
-# Adjust variables to the width and height of the screen
 def width_adjust(x: int) -> int:
-    """
-    Adjust variables to the width of the screen
-    """
+    """Adjust variables to the width of the screen."""
     return int(x/1920 * SCREEN_WIDTH)
 
 def height_adjust(y: int) -> int:
-    """
-    Adjust variables to the height of the screen
-    """
+    """Adjust variables to the height of the screen."""
     return int(y/1080 * SCREEN_HEIGHT)
 
-# Dimensions of bottom dialogue option.
+# Dimensions of bottom dialogue option
 BOTTOM_DIALOGUE_MIN_X = width_adjust(1300)
 BOTTOM_DIALOGUE_MAX_X = width_adjust(1700)
 BOTTOM_DIALOGUE_MIN_Y = height_adjust(790)
 BOTTOM_DIALOGUE_MAX_Y = height_adjust(800)
 
-# Pixel coordinates for white part of the autoplay button.
+# Pixel coordinates for white part of the autoplay button
 PLAYING_ICON_X = width_adjust(84)
 PLAYING_ICON_Y = height_adjust(46)
 
-# Pixel coordinates for white part of the speech bubble in bottom dialogue option.
+# Pixel coordinates for white part of the speech bubble in bottom dialogue option
 DIALOGUE_ICON_X = width_adjust(1301)
 DIALOGUE_ICON_LOWER_Y = height_adjust(808)
 DIALOGUE_ICON_HIGHER_Y = height_adjust(790)
 
-# Pixel coordinates near middle of the screen known to be white while the game is loading.
+# Pixel coordinates near middle of the screen known to be white while the game is loading
 LOADING_SCREEN_X = width_adjust(1200)
 LOADING_SCREEN_Y = height_adjust(700)
 
