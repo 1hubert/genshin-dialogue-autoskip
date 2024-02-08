d704d83abfe0521ae2f4e81162b3806deab6dcca
Hubert Rozmarynowski <hrozmarynowski776@gmail.com>
2024-02-08 21:53:51 +0100

    delete kbm-specific code

--- autoskip_dialogue.py
+++ autoskip_dialogue.py
@@ -8,12 +8,6 @@ import pyautogui
 from pynput.mouse import Controller
 from pynput.keyboard import Key, KeyCode, Listener
 
-# Dimensions of bottom dialogue option.
-BOTTOM_DIALOGUE_MIN_X: int = 1300
-BOTTOM_DIALOGUE_MAX_X: int = 1700
-BOTTOM_DIALOGUE_MIN_Y: int = 790
-BOTTOM_DIALOGUE_MAX_Y: int = 800
-
 # Pixel coordinates for pink pixel of the autoplay button (DualShock 4 Square and Xbox "X"). eng
 DS4_ENG_AUTOPLAY_ICON_X: int = 1450
 DS4_ENG_AUTOPLAY_ICON_Y: int = 1010
@@ -86,18 +80,6 @@ def random_interval() -> float:
     """
 
     return uniform(0.18, 0.3) if randrange(1, 7) == 6 else uniform(0.12, 0.18)
-
-
-def random_cursor_position() -> Tuple[int, int]:
-    """
-    The cursor is moved to a random position in the bottom dialogue option.
-    :return: A random (x, y) in range of the bottom dialogue option.
-    """
-
-    x: int = randrange(BOTTOM_DIALOGUE_MIN_X, BOTTOM_DIALOGUE_MAX_X + 1)
-    y: int = randrange(BOTTOM_DIALOGUE_MIN_Y, BOTTOM_DIALOGUE_MAX_Y + 1)
-
-    return x, y
 
 
 def exit_program() -> None:
@@ -206,15 +188,12 @@ def press_cross(gamepad: Union[vg.VDS4Gamepad, vg.VX360Gamepad]) -> None:
 
 
 def main() -> None:
-
     """
     Skip Genshin Impact dialogue when it's present based on the colors of some specific pixels.
     :return: None
     """
 
     main.status = 'pause'
-    last_reposition: float = 0.0
-    time_between_repositions: float = random_interval() * 40
     ds4_gamepad = vg.VDS4Gamepad()
     xbox_gamepad = vg.VX360Gamepad()
 
